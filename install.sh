4ed6c621a40604f10a186a06b1ce0b1e6887b3dd
Sakura-Sou <43777692+Sakura-Sou@users.noreply.github.com>
2019-01-25 22:51:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
+cp rclone-*-linux-amd64/rclone .
 docker build -t rclone:v0.1 .
